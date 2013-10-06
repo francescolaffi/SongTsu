3d86a7ea1f9c374c1c424523130e667df8219528
Yuri Valentini <yuroller@gmail.com>
2013-10-06 10:07:58 +0200

    Tidy up javascript

--- js-commons/svg-matrix.js
+++ js-commons/svg-matrix.js
@@ -1,9 +1,13 @@
 (function ($) {
+    'use strict';
     function Matrix(el, dimension, opt) {
         var defaults = {
             min: 200,
             max: 1023
-        };
+        },
+            svg,
+            col,
+            row;
 
         this.el = el;
         this.opt = $.extend({}, defaults, opt);
@@ -14,19 +18,20 @@
         svg.size('100%', '100%');
         svg.viewbox(0, 0, dimension, dimension);
 
-        this.rects = Array(dimension);
-        for (col = this.dimension - 1; col >= 0; col--) {
-            this.rects[col] = Array(dimension);
-            for (row = this.dimension - 1; row >= 0; row--) {
-                this.rects[col][row] =  svg.rect(1,1).move(col, row);
+        this.rects = new Array(dimension);
+        for (col = this.dimension - 1; col >= 0; col = col - 1) {
+            this.rects[col] = new Array(dimension);
+            for (row = this.dimension - 1; row >= 0; row = row - 1) {
+                this.rects[col][row] = svg.rect(1,1).move(col, row);
                 this.rects[col][row].animate().fill({opacity: 0});
             }
         }
     }
 
     Matrix.prototype.setData = function (data) {
-        for (col = this.dimension - 1; col >= 0; col--) {
-            for (row = this.dimension - 1; row >= 0; row--) {
+        var row, col, v;
+        for (col = this.dimension - 1; col >= 0; col = col - 1) {
+            for (row = this.dimension - 1; row >= 0; row = row - 1) {
                 v = Math.max(0, Math.min(this.opt.max, data[col][row]));
                 v = (v - this.opt.min) / (this.opt.max - this.opt.min);
                 this.rects[col][row].fill({opacity: v});
