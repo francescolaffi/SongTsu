cc88f90679c478bef81af8116c7506007ad3d988
Yuri Valentini <yuroller@gmail.com>
2013-10-14 12:23:58 +0200

    reorganized openpicus project

--- openpicus/UpTsu/Libs/Flyport libs/Include/WFlib.h
+++
@@ -1,9 +0,0 @@
-/*******************************
- * openPicus Core Team
- * IDE 2.2 compatibility for
- * old WFlib.h includes
- *
- * Simone Marra 2012/04/11
- *******************************/
-
-#include "NETlib.h"
